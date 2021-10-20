95bf3a9952c0b53366c335528a8cb6f76a76227a
hwookim <gusdn9754@gmail.com>
2021-10-20 22:01:15 +0900

    chore: Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+  stories: ['../__stories__/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
