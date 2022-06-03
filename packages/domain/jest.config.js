5cd8df413db02a71379f23c8801c3849767ffee9
hwookim <gusdn9754@gmail.com>
2022-06-03 15:23:46 +0900

    #1 chore: tsconfig, jest 설정

---
+++ packages/domain/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/__test__/$1',
+  },
+  testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
