f183ea0fcec4a6de736ad941db41912874a57b4d
hwookim <gusdn9754@gmail.com>
2022-06-26 17:21:32 +0900

    #1 fix: 시간값 균일하게 정오로 고정

--- packages/domain/src/utils/index.ts
+++ packages/domain/src/utils/index.ts
@@ -3,5 +3,8 @@ export function getDayDateInWeek(day: number): Date {
     throw new Error('요일 0(일요일)~6(토요일) 사이로 입력해야합니다.');
   }
   const today = new Date();
-  return new Date(new Date().setDate(today.getDate() - today.getDay() + day));
+  const result = new Date();
+  result.setDate(today.getDate() - today.getDay() + day);
+  result.setHours(0, 0, 0, 0);
+  return result;
 }
